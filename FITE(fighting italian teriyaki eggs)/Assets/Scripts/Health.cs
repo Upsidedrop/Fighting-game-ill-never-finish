a4b1530712517fe45ba797ef36b45b5298895633
Upsidedrop <96622429+Upsidedrop@users.noreply.github.com>
2023-07-06 14:04:43 -0400

    Update 1.04
    Added health bar

--- FITE(fighting italian teriyaki eggs)/Assets/Scripts/Health.cs
+++ FITE(fighting italian teriyaki eggs)/Assets/Scripts/Health.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField]
+    Slider healthSlider;
     public float health = 100;
+    private void Awake()
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
     public void Damage(float damage)
     {
         health -= damage;
-        print(health);
+        healthSlider.value = health;
     }
     private void Update()
     {
         if (health == 0 || health < 0)
         {
             print("へ（>_<へ)");
+
         }
+
     }
 }
